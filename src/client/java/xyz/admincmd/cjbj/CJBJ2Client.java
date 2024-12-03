f30d208b3b9769f055b310ed41cc041eaeb9718e
admincmd-a <3374235679@qq.com>
2024-12-03 21:44:50 +0800

    chongongh

---
+++ src/client/java/xyz/admincmd/cjbj/CJBJ2Client.java
@@ -0,0 +1,10 @@
+package xyz.admincmd.cjbj;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CJBJ2Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/xyz/admincmd/cjbj/CJBJ2DataGenerator.java
@@ -0,0 +1,11 @@
+package xyz.admincmd.cjbj;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CJBJ2DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
